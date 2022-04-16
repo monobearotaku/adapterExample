30a1f1a97b7aac6141d8bf422789927ad341cb7a
r.y.nikolaev <r.y.nikolaev@tinkoff.ru>
2022-04-16 21:32:54 +0300

    First commit

--- math/math.go
+++ math/math.go
@@ -1,12 +1,15 @@
 package math
 
 func Square(x int) int {
-	return x * x
+	/*
+		INSERT YOUR CODE
+	*/
+	return 0
 }
 
 func Cube(x int) int {
 	/*
 		INSERT YOUR CODE
 	*/
-	return x * x * (-x)
+	return 0
 }
